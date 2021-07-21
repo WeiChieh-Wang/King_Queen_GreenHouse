ab1ad5c99ef59002dbd20c2939620c12e8f56a02
WeiChieh-Wang <75101293+WeiChieh-Wang@users.noreply.github.com>
2021-07-21 13:57:09 +0800

    Add files via upload

--- scripts/account.js
+++ scripts/account.js
@@ -1,11 +1,14 @@
-let valnm,valem,valpw;
+let name_val,email_val,password_val;
 let data1;
-let nm=[],em=[],pw=[],mn=[],unm=[];
+let name_push=[],email_push=[],password_push=[],manager_push=[],username_push=[];
 var i=0;
 var o=document.getElementById("sign");
+let username_push_flat_com=[], password_push_flat_com=[];
+
+
 function preload(){
   btn= document.getElementById("submit");
-    data1=loadJSON('https://spreadsheets.google.com/feeds/list/1S5Emd0mzEf96JQc2OR17NzqQUJB8jwwMek0k4ZA2iWo/od6/public/values?alt=json')
+  data1=loadJSON('https://spreadsheets.google.com/feeds/list/1S5Emd0mzEf96JQc2OR17NzqQUJB8jwwMek0k4ZA2iWo/od6/public/values?alt=json')
   var o=document.getElementById("sign");
   user1=window.localStorage.getItem('email');
   if(user1==null){
@@ -15,6 +18,7 @@ function preload(){
     o.style.display='none';
   }
 }
+
 function out(){
   var o=document.getElementById("sign");
   localStorage.removeItem('email');
@@ -25,36 +29,37 @@ function out(){
   o.style.display='show';
   window.location.href='../index.html';
 }
- function setup(){
-    data1.feed.entry.forEach((b)=>{
-    unm.push([b.gsx$username.$t]);
-    nm.push([b.gsx$name.$t]);
-    em.push([b.gsx$email.$t]);
-    pw.push([b.gsx$password.$t]);
-    mn.push([b.gsx$管理者積分.$t]);
-   })
-  let fn = nm.flat(Infinity);
-  let fun = unm.flat(Infinity);
-  let fe = em.flat(Infinity);
-  let fp = pw.flat(Infinity);
-  let fm = mn.flat(Infinity);
+
+function setup(){
+  data1.feed.entry.forEach((b)=>{
+    username_push.push([b.gsx$username.$t]);
+    name_push.push([b.gsx$name.$t]);
+    email_push.push([b.gsx$email.$t]);
+    password_push.push([b.gsx$password.$t]);
+    manager_push.push([b.gsx$管理者積分.$t]);
+  })
+  let name_push_flat = name_push.flat(Infinity);
+  let username_push_flat = username_push.flat(Infinity);
+  let email_push_flat = email_push.flat(Infinity);
+  let password_push_flat = password_push.flat(Infinity);
+  let manager_push_flat = manager_push.flat(Infinity);
   btn.addEventListener('click',()=>{
-    valunm_o= document.getElementById("username").value;
+    username_val= document.getElementById("username").value;
     //valem= document.getElementById("email").value;
-    valpw_o= document.getElementById("password").value;
-    valunm = decode(valunm_o);
-    valpw = decode(valpw_o);
-    a=fun.indexOf(valunm) ;
+    password_val= document.getElementById("password").value;
+    username_push_flat_com = decode(username_push_flat);
+    password_push_flat_com = decode(password_push_flat);
+    a=username_push_flat_com.indexOf(username_val) ;
     //b=fe.indexOf(valem);
-    c=fp.indexOf(valpw); 
+    c=password_push_flat_com.indexOf(password_val); 
     if(a>-1){
       if (a==c){    
         console.log("輸入成功");
-        window.localStorage.setItem('manager',fm[a]);
-        window.localStorage.setItem('email',fe[a]);
-        window.localStorage.setItem('name',fn[a]);
-        window.localStorage.setItem('password',valpw);
-        window.localStorage.setItem('username',valunm);
+        window.localStorage.setItem('manager',manager_push_flat[a]);
+        window.localStorage.setItem('email',email_push_flat[a]);
+        window.localStorage.setItem('name',name_push_flat[a]);
+        window.localStorage.setItem('password',password_val);
+        window.localStorage.setItem('username',username_val);
         console.log(window.localStorage.getItem('manager'));
         alert('登入成功');
         window.location.href='../index.html';
@@ -63,13 +68,11 @@ function out(){
         alert('密碼錯誤');
       }
     }
-   if(a==-1){
-    alert('輸入錯誤');
-   }
-})}
-  
-
-function draw(){}
+    if(a==-1){
+      alert('輸入錯誤');
+    }
+  })
+}
 
 function decode(content){
   var de = "";
